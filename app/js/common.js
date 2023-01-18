135b4f7856c147a76cb5e5a6927dc1c8f22ee136
Trikita73 <diachenkoandrii7@gmail.com>
2023-01-18 12:34:29 +0200

    map

--- app/js/common.js
+++ app/js/common.js
@@ -638,6 +638,17 @@ function filterRange(arr, a, b) {
 }
 */
 
+/* Напишите функцию camelize(str), которая преобразует строки вида 'my-short-string' в 'myShortString'.
+То есть дефисы удаляются, а все слова после них получают заглавную букву. */
+
+/*
+let camelize = prompt('Enter', 'Font-back-string');
+
+function camelize(str) {
+	return str.split('-').map((item, index) => index == 0 ? item : item[0].toUpperCase() + item.slice(1)).join('');
+}
+*/
+
 
 
 
