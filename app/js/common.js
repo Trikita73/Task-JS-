2f946169345715d043fc57a38f573a21673e226e
Trikita73 <diachenkoandrii7@gmail.com>
2023-05-31 16:59:19 +0300

    task_3_DOM

--- app/js/common.js
+++ app/js/common.js
@@ -964,4 +964,80 @@ b) Правда, что elem.children[0].previousSibling всегда равен
 
 //elem.lastChild.nextSibling // Yes(null)
 
+/* Task_1_b */
+
 //elem.children[0].previousSibling // No(null)
+
+/* Task_3 */
+
+/*
+Выделите ячейки по диагонали
+Напишите код, который выделит красным цветом все ячейки в таблице по диагонали.
+Вам нужно получить из таблицы <table> все диагональные <td> и выделить их, используя код:
+
+//  в переменной td находится DOM-элемент для тега <td>
+td.style.backgroundColor = 'red';
+
+<!DOCTYPE HTML>
+<html>
+<head>
+  <style>
+    table {
+      border-collapse: collapse;
+    }
+
+    td {
+      border: 1px solid black;
+      padding: 3px 5px;
+    }
+  </style>
+</head>
+
+<body>
+  <table>
+    <tr>
+      <td>1:1</td>
+      <td>2:1</td>
+      <td>3:1</td>
+      <td>4:1</td>
+      <td>5:1</td>
+    </tr>
+    <tr>
+      <td>1:2</td>
+      <td>2:2</td>
+      <td>3:2</td>
+      <td>4:2</td>
+      <td>5:2</td>
+    </tr>
+    <tr>
+      <td>1:3</td>
+      <td>2:3</td>
+      <td>3:3</td>
+      <td>4:3</td>
+      <td>5:3</td>
+    </tr>
+    <tr>
+      <td>1:4</td>
+      <td>2:4</td>
+      <td>3:4</td>
+      <td>4:4</td>
+      <td>5:4</td>
+    </tr>
+    <tr>
+      <td>1:5</td>
+      <td>2:5</td>
+      <td>3:5</td>
+      <td>4:5</td>
+      <td>5:5</td>
+    </tr>
+  </table>
+</body>
+</html>
+*/
+
+let table = document.body.firstElementChild;
+
+for(let i = 0; i < table.rows.lenght; i++) {
+	let row = table.rows[i];
+	row.cells[i].style.backgroungColor = 'red';
+}
