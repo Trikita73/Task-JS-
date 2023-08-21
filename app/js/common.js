c53431b52c9014ea6401344b9b90ccf50991a694
Trikita73 <diachenkoandrii7@gmail.com>
2023-08-22 01:59:42 +0300

    task_12_DOM

--- app/js/common.js
+++ app/js/common.js
@@ -1435,5 +1435,103 @@ function makeFriendslist(friends) {
 	
 	return ul;
 }
+*/ 
+
+/* Task_12*/
+
+/*
+Напишите функцию makeDiagonalRed, которая выделит красным цветом все ячейки в таблице по диагонали.
+
+Вам нужно получить из таблицы <table> все диагональные <td> и выделить их красным. 
+
+function makeDiagonalRed(table) {
+  // ваш код
+}
+
+// Где в переменной table находится DOM-элемент для тега <table>
+makeDiagonalRed(table);
+
+P.S. для решения рекомендуется использовать свойства rows таблицы (элемент <table>) и cells элемента строки (элемент <tr>).
+
+Чтобы сделать ячейку красной, рекомендуется использовать свойство style, которое есть у всех элементов.
+*/
+
+/*
+
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  <style>
+    table {
+      border-collapse: collapse;
+    }
+
+    td {
+      border: 1px solid black;
+      padding: 3px 5px;
+    }
+  </style>
+</head>
+
+<body>
+  <table>
+    <tr>
+      <td>1:1</td>
+      <td>2:1</td>
+      <td>3:1</td>
+      <td>4:1</td>
+      <td>5:1</td>
+    </tr>
+    <tr>
+      <td>1:2</td>
+      <td>2:2</td>
+      <td>3:2</td>
+      <td>4:2</td>
+      <td>5:2</td>
+    </tr>
+    <tr>
+      <td>1:3</td>
+      <td>2:3</td>
+      <td>3:3</td>
+      <td>4:3</td>
+      <td>5:3</td>
+    </tr>
+    <tr>
+      <td>1:4</td>
+      <td>2:4</td>
+      <td>3:4</td>
+      <td>4:4</td>
+      <td>5:4</td>
+    </tr>
+    <tr>
+      <td>1:5</td>
+      <td>2:5</td>
+      <td>3:5</td>
+      <td>4:5</td>
+      <td>5:5</td>
+    </tr>
+  </table>
+  <script>
+
+    let table = document.querySelector('table');
+
+		function makeDiagonalRed(table) {
+
+			let rowsLength = table.rows.length;
+			let rows = table.rows;
+		
+			for (let i = 0; i < rowsLength; i++) {
+			let row = rows[i];
+			row.cells[i].style.backgroundColor = 'red';
+			}
+		}
+
+    makeDiagonalRed(table);
+	
+  </script>
+</body>
+
+</html>
 */
 
