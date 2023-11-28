337be5c52591fe625adbc727309e9e0cf269af71
Trikita73 <diachenkoandrii7@gmail.com>
2023-11-28 23:48:31 +0200

    mouse_click_task_1

--- app/js/common.js
+++ app/js/common.js
@@ -2933,3 +2933,89 @@ P.S. В один момент может быть показана только 
 </body>
 </html>
 */
+
+
+/* TASK_16 */
+
+/*
+Выделяемый список
+Создайте список, в котором элементы могут быть выделены, как в файловых менеджерах.
+При клике на элемент списка выделяется только этот элемент (добавляется класс .selected), отменяется выделение остальных элементов.
+Если клик сделан вместе с Ctrl (Cmd для Mac), то выделение переключается на элементе, но остальные элементы при этом не изменяются.
+*/
+
+/*
+P.S. В этом задании все элементы списка содержат только текст. Без вложенных тегов.
+P.P.S. Предотвратите стандартное для браузера выделение текста при кликах.
+*/
+
+// <<<< решение:
+
+/*
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <style>
+    .selected {
+      background: #0f0;
+    }
+
+    li {
+      cursor: pointer;
+    }
+  </style>
+</head>
+
+<body>
+
+  Кликни на элемент списка, чтобы выделить его.
+  <br>
+
+  <ul id="ul">
+    <li>Кристофер Робин</li>
+    <li>Винни Пух</li>
+    <li>Тигра</li>
+    <li>Кенга</li>
+    <li>Кролик. Просто Кролик.</li>
+  </ul>
+
+  <script>
+    ul.onclick = function(event) {
+      if (event.target.tagName != "LI") return;
+
+      if (event.ctrlKey || event.metaKey) {
+        toggleSelect(event.target);
+      } else {
+        singleSelect(event.target);
+      }
+
+    }
+
+    // предотвращает ненужное выделение элементов списка при клике
+    ul.onmousedown = function() {
+      return false;
+    };
+
+    function toggleSelect(li) {
+      li.classList.toggle('selected');
+    }
+
+    function singleSelect(li) {
+      let selected = ul.querySelectorAll('.selected');
+      for(let elem of selected) {
+        elem.classList.remove('selected');
+      }
+      li.classList.add('selected');
+    }
+
+  </script>
+
+</body>
+</html>
+*/
+
+
+
+
