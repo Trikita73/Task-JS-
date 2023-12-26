cf9df795c0822cce98558eb82edf5f89a39d4c13
Trikita73 <diachenkoandrii7@gmail.com>
2023-12-26 16:07:44 +0200

    task_2.1

---
+++ app/js/index1.js
@@ -0,0 +1,36 @@
+import createElement from '../libs/lib/create-element.js';
+import escapeHtml from '../libs/lib/escape-html.js';
+
+export default class ProductCard {
+    constructor(product) {
+        this.product = product;
+        this.render();
+        this.addEventlisteners();
+    }
+
+    render() {
+        this.elem = createElement(`<div class="card">
+            <div class="card__top">
+                <img src="/img/products/${this.product.image}" class="card__image" alt="product"/>
+                <span class="card__price">€${this.product.price.toFixed(2)}</span>
+            </div>
+            <div class="card__body">
+                <div class="card__title">${escapeHtml(this.product.name)}</div>
+                <button type="button" class="card__button">
+                    <img src="/img/icons/plus-icon.svg" alt="icon"/>
+                </button>
+            </div>
+        </div>`);
+    }
+
+    addEventlisteners() {
+        this.elem.onclick = (event) => this.onclick(event);
+    }
+
+    onClick(event) {
+        this.elem.dispatchEvent(new CustomEvent("product-add", {
+            detail: this.product.id,
+            bubbles: true
+        }));
+    }
+}
