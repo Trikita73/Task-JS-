3d5ff637cea41a01bb7bd2a7a39377504850740e
Trikita73 <diachenkoandrii7@gmail.com>
2024-01-29 15:47:38 +0200

    modal_window_1.0

--- app/js/index4.js
+++ app/js/index4.js
@@ -1,4 +1,4 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from '../libs/lib/create-elements.js';
 
 export default class Modal {
     constructor() {
@@ -14,11 +14,58 @@ export default class Modal {
                 <div class="modal__inner">
                     <div class="modal__header">
                         <button type="button" class="modal__close">
-                            
+                            <img src ="/img/icons/cross-icon.svg" alt="close-icon" />
                         </button>
+                        <h3 class="modal__title"><h3>
                     </div>
+                    <div clas="modal__body"></div>
                 </div>
             </div>
         `);
+    }
+
+    sub(ref) {
+        return this.elem.querySelector(`.modal__${ref}`);
+    }
+
+    open() {
+        document.body.append(this.elem);
+        document.body.classList('is-modal-open');
+        
+        this._keydownEventListener = (event) => this.onDocumentKeyDown(event);
+        document.addEventListener('keydown', this._keydownEventListener);
+
+        if (this.elem.querySelector('[autofocus]')) {
+            this.elem.querySelector('[autofocus]').focus();
+        }
+    }
+
+    onClick(event) {
+        if (event.target.closest('.modal__close')) {
+            event.preventDefault();
+            this.close();
+        }
+    }
+
+    onDocumentKeyDown(event) {
+        if (event.code === 'Escape') {
+            event.preventDefault();
+            this.close();
+        }
+    }
+
+    setTitle(title) {
+        this.sub('title').textContent = title;
+    }
+
+    setBody(node) {
+        this.sub('body').innerHTML = '';
+        this.sub('body').append(node);
+    }
+
+    close() {
+        document.removeEventListener('keydown', this._keydownEventListener);
+        document.body.classList.remove('is-modal-open');
+        this.elem.remove();
     }
 }
