5446b5ce7770f970cfa006d2a67d948e9810bb41
Trikita73 <diachenkoandrii7@gmail.com>
2024-02-05 16:56:16 +0200

    task_3

--- app/js/common.js
+++ app/js/common.js
@@ -7017,3 +7017,241 @@ INDEX.HTML:
 
 </body>
 */
+
+
+/* TASK_3 */
+
+/*
+Редактирование TD по клику
+
+Сделайте ячейки таблицы редактируемыми по клику.
+
+По клику – ячейка должна стать «редактируемой» (textarea появляется внутри), 
+мы можем изменять HTML. Изменение размера ячейки должно быть отключено.
+
+Кнопки OK и ОТМЕНА появляются ниже ячейки и, соответственно, 
+завершают/отменяют редактирование.
+
+Только одну ячейку можно редактировать за один раз. Пока <td> в 
+«режиме редактирования», клики по другим ячейкам игнорируются.
+
+Таблица может иметь множество ячеек. Используйте делегирование событий.
+
+1) По клику – заменить innerHTML ячейки на <textarea> с теми же размерами и без рамки. 
+Можно использовать JavaScript или CSS, чтобы установить правильный размер.
+
+2) Присвоить textarea.value значение td.innerHTML.
+
+3) Установить фокус на текстовую область.
+
+4) Показать кнопки ОК/ОТМЕНА под ячейкой, обрабатывать клики по ним.
+*/
+
+// <<<< решение:
+
+/*
+<style>
+// общие стили для таблицы, изменять их не нужно 
+
+th {
+text-align: center;
+font-weight: bold;
+}
+
+td {
+width: 150px;
+white-space: nowrap;
+text-align: center;
+vertical-align: middle;
+padding: 10px;
+}
+
+.nw {
+background-color: #999;
+}
+
+.n {
+background-color: #03f;
+color: #fff;
+}
+
+.ne {
+background-color: #ff6;
+}
+
+.w {
+background-color: #ff0;
+}
+
+.c {
+background-color: #60c;
+color: #fff;
+}
+
+.e {
+background-color: #09f;
+color: #fff;
+}
+
+.sw {
+background-color: #963;
+color: #fff;
+}
+
+.s {
+background-color: #f60;
+color: #fff;
+}
+
+.se {
+background-color: #0c3;
+color: #fff;
+}
+// add css 
+.edit-td .edit-area {
+border: none;
+margin: 0;
+padding: 0;
+display: block;
+
+// отключаем изменение размера мышью в Firefox 
+resize: none;
+
+// удаляем обводку при фокусировке в Chrome 
+outline: none;
+
+// удаляем полосу прокрутки в IE 
+overflow: auto;
+}
+
+.edit-controls {
+position: absolute;
+}
+
+.edit-td {
+position: relative;
+padding: 0;
+}
+</style>
+
+<p>Кликните на ячейку таблицы, чтобы отредактировать её. Нажмите ОК или ОТМЕНА, когда закончите.</p>
+
+<table id="bagua-table">
+	<tr>
+	<th colspan="3">Квадрат <em>Bagua</em>: Направление, Элемент, Цвет, Значение</th>
+	</tr>
+	<tr>
+	<td class="nw"><strong>Северо-Запад</strong>
+		<br>Металл
+		<br>Серебро
+		<br>Старейшины
+	</td>
+	<td class="n"><strong>Север</strong>
+		<br>Вода
+		<br>Синий
+		<br>Перемены
+	</td>
+	<td class="ne"><strong>Северо-Восток</strong>
+		<br>Земля
+		<br>Жёлтый
+		<br>Направление
+	</td>
+	</tr>
+	<tr>
+	<td class="w"><strong>Запад</strong>
+		<br>Металл
+		<br>Золото
+		<br>Молодость
+	</td>
+	<td class="c"><strong>Центр</strong>
+		<br>Всё
+		<br>Пурпурный
+		<br>Гармония
+	</td>
+	<td class="e"><strong>Восток</strong>
+		<br>Дерево
+		<br>Синий
+		<br>Будущее
+	</td>
+	</tr>
+	<tr>
+	<td class="sw"><strong>Юго-Запад</strong>
+		<br>Земля
+		<br>Коричневый
+		<br>Спокойствие
+	</td>
+	<td class="s"><strong>Юг</strong>
+		<br>Огонь
+		<br>Оранжевый
+		<br>Слава
+	</td>
+	<td class="se"><strong>Юго-Восток</strong>
+		<br>Дерево
+		<br>Зеленый
+		<br>Роман
+	</td>
+	</tr>
+
+</table>
+
+<script>
+	let table = document.getElementById('bagua-table');
+
+	let editingTd;
+
+	table.onclick = function(event) {
+
+		// 3 возможных цели
+		let target = event.target.closest('.edit-cancel, .edit-ok,td');
+
+		if (!table.contains(target)) return;
+
+		if (target.className == 'edit-cancel') {
+			finishTdEdit(editingTd.elem, false);
+		}	else if (target.className == 'edit-ok') {
+			finishTdEdit(editingTd.elem, true);
+		}	else if (target.nodeName == 'TD') {
+			if (editingTd) return; // уже редактируется
+
+			makeTdEdittable(target);
+		}
+	};
+
+	function makeTdEdittable(td) {
+		editingTd = {
+			elem: td,
+			data: td.innerHTML
+		};
+
+		td.classList.add('edit-td'); // td в состоянии редактирования, CSS применятся к textarea внутри ячейки
+
+		let textArea = document.createElement('textarea');
+		textArea.style.width = td.clientWidth + 'px';
+		textArea.style.height = td.clientHeight + 'px';
+		textArea.className = 'edit-area';
+
+		textArea.value = td.innerHTML;
+		td.innerHTML = '';
+		td.appendChild(textArea);
+		textArea.focus();
+
+		td.insertAdjacentHTML("beforeEnd",
+			'<div class="edit-controls"><button class="edit-ok">OK</button><button class="edit-cancel">CANCEL</button></div>'
+		);
+	}
+
+	function finishTdEdit(td, isOk) {
+		if (isOk) {
+			td.innerHTML = td.firstChild.value;
+		}	else {
+			td.innerHTML = editingTd.data;
+		}
+		td.classList.remove('edit-td');
+		editingTd = null;
+	}
+</script>
+
+</body>
+
+</html>
+*/
