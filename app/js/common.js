4ab0f25dc441e29b1a55351b55eef105ecdffa5b
Trikita73 <diachenkoandrii7@gmail.com>
2024-02-09 13:28:53 +0200

    task_10

--- app/js/common.js
+++ app/js/common.js
@@ -7910,11 +7910,89 @@ radius – радиус круга.
 /* TASK_10 */
 
 /*
+Анимация круга с помощью колбэка
 
+В задаче Анимированный круг(task_9) показывается анимированный 
+растущий круг.
+
+Теперь предположим, что нам нужен не просто круг, а чтобы в нём 
+было ещё и сообщение. Сообщение должно появиться после завершения 
+анимации (круг полностью вырос), в противном случае это будет 
+выглядеть некрасиво.
+
+В решении задачи функция showCircle(cx, cy, radius) рисует окружность, 
+но не даёт возможности отследить, когда она будет готова.
+
+В аргументы добавьте колбэк: showCircle(cx, cy, radius, callback) 
+который будет вызываться по завершении анимации. Колбэк в качестве 
+аргумента должен получить круг <div>.
+
+Вот пример:
+
+showCircle(150, 150, 100, div => {
+  div.classList.add('message-ball');
+  div.append("Hello, world!");
+});
 */
 
 // <<<< решение:
 
 /*
+<!DOCTYPE html>
+<html>
 
+<head>
+  <meta charset="utf-8">
+  <style>
+	.message-ball {
+      font-size: 20px;
+      line-height: 200px;
+      text-align: center;
+    }
+    .circle {
+      transition-property: width, height;
+      transition-duration: 2s;
+      position: fixed;
+      transform: translateX(-50%) translateY(-50%);
+      background-color: red;
+      border-radius: 50%;
+    }
+  </style>
+</head>
+
+<body>
+
+	<button onclick="go()">Нажми на меня</button>
+
+	<script>
+
+		function go() {
+			showCircle(150, 150, 100, div => {
+				div.classList.add('message-ball');
+				div.append("Привет, мир!")
+			});
+		}
+
+		function showCircle(cx, cy, radius, callback) {
+			let div = document.createElement('div');
+			div.style.width = 0;
+			div.style.height = 0;
+			div.style.left = cx + 'px';
+			div.style.top = cy + 'px';
+			div.className = 'circle';
+			document.body.append(div);
+
+			setTimeout(() => {
+				div.style.width = radius * 2 + 'px';
+				div.style.height = radius * 2 + 'px';
+
+				div.addEventListener('transitionend', function handler() {
+					div.removeEventListener('transitionend', handler);
+					callback(div);
+				});
+			}, 0);
+		}
+	</script>
+</body>
+</html>
 */
