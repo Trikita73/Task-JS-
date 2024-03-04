06d382f963f8e437badf0b819637e2e41d544a0d
Trikita73 <diachenkoandrii7@gmail.com>
2024-03-04 16:13:55 +0200

    task_7

--- app/js/common.js
+++ app/js/common.js
@@ -10704,3 +10704,74 @@ promise
 останется необработанной.
 */
 
+/* TASK_7 */
+
+/*
+Перепишите, используя async/await
+
+Перепишите один из примеров раздела Цепочка промисов, 
+используя async/await вместо .then/catch:
+
+function loadJson(url) {
+  return fetch(url)
+    .then(response => {
+      if (response.status == 200) {
+        return response.json();
+      } else {
+        throw new Error(response.status);
+      }
+    })
+}
+
+loadJson('no-such-user.json') // (3)
+  .catch(alert); // Error: 404
+*/
+
+// <<<< решение:
+
+/*
+async function loadJson(url) { // (1)
+  	let response = await fetch(url); // (2)
+
+  	if (response.status == 200) {
+    	let json = await response.json(); // (3)
+    	return json;
+  	}
+
+  	throw new Error(response.status);
+}
+
+loadJson('no-such-user.json')
+  .catch(alert); // Error: 404 (4)
+
+Комментарии:
+
+1) Функция loadJson теперь асинхронная.
+2) Все .then внутри неё заменены на await.
+3) Можно было бы просто вернуть промис во внешний код 
+return response.json(), вот так:
+
+if (response.status == 200) {
+  return response.json(); // (3)
+}
+
+Тогда внешнему коду пришлось бы получать результат промиса 
+самостоятельно (через .then или await). В нашем варианте это не 
+обязательно
+
+4) Выброшенная из loadJson ошибка перехватывается с помощью .catch. 
+Здесь нельзя использовать await loadJson(…), так как мы находимся 
+не в теле функции async.
+*/
+
+/* TASK_8 */
+
+/*
+
+*/
+
+// <<<< решение:
+
+/*
+
+*/
