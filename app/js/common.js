89e6af184688599e6d0fef32562bc9189edf2ef9
Trikita73 <diachenkoandrii7@gmail.com>
2024-03-09 02:35:53 +0200

    task_12

--- app/js/common.js
+++ app/js/common.js
@@ -11042,4 +11042,40 @@ preloadImages(["1.jpg", "2.jpg", "3.jpg"], loaded);
 </script>
 */
 
+/* TASK_12 */
+
+/*
+Странный instanceof
+
+Почему instanceof в примере ниже возвращает true? 
+Мы же видим, что a не создан с помощью B().
+
+function A() {}
+function B() {}
+
+A.prototype = B.prototype = {};
+
+let a = new A();
+
+alert(a instanceof B);
+
+*/
+
+// <<<< решение:
+
+/*
+Да, действительно, выглядит странно.
+
+Но instanceof не учитывает саму функцию при проверке, 
+а только prototype, который проверяется на совпадения в 
+прототипной цепочке.
+
+И в данном примере a.__proto__ == B.prototype, так что 
+instanceof возвращает true.
+
+Таким образом, по логике instanceof, именно prototype в 
+действительности определяет тип, а не функция-конструктор.
+*/
+
+
 
