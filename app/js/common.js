e5990b3d1783368f7859de7b798a1e25b71f7e42
Trikita73 <diachenkoandrii7@gmail.com>
2024-03-11 15:31:32 +0200

    task_14

--- app/js/common.js
+++ app/js/common.js
@@ -11139,3 +11139,63 @@ export default function promiseClick(button) {
 	});
 }
 */
+
+/* TASK_14 */
+
+/*
+Декоратор-шпион
+
+Создайте декоратор spy(func), который должен возвращать обёртку, 
+которая сохраняет все вызовы функции в своём свойстве calls.
+
+Каждый вызов должен сохраняться как массив аргументов.
+
+Например:
+
+function work(a, b) {
+  alert( a + b ); // произвольная функция или метод
+}
+
+work = spy(work);
+
+work(1, 2); // 3
+work(4, 5); // 9
+
+for (let args of work.calls) {
+  alert( 'call:' + args.join() ); // "call:1,2", "call:4,5"
+}
+*/
+
+// <<<< решение:
+
+/*
+Доп файлы: 
+testcallapply.js;
+path: /js/testcallapply.js;
+
+Обертка, возвращаемая spy(f), должна хранить все аргументы, 
+и затем использовать f.apply для переадресации вызова.
+
+HTML: 
+<!doctype html>
+<script src="https://ru.js.cx/test/libs.js"></script>
+<script src="js/testcallapply1.js"></script>
+<script>
+
+	function spy(func) {
+		function wrapper(...args) {
+			// мы используем ...args вместо arguments для хранения "реального" массива в wrapper.calls
+			wrapper.calls.push(args);
+			return func.apply(this, args);
+		}
+
+		wrapper.calls = [];
+
+		return wrapper;
+	}
+
+</script>
+
+</html>
+*/
+
